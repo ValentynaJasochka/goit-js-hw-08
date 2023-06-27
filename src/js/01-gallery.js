a900c0690510738abeafd7d191b189ffa4b4d7cc
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2023-06-27 19:07:52 +0300

    galery set

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+function createMarkupList(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        ` <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    )
+    .join(' ');
+}
+list.insertAdjacentHTML('afterbegin', createMarkupList(galleryItems));
+
+// // use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
