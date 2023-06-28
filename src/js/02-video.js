7152c1371fb5942cb4849154acba4cec4700d90e
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2023-06-28 23:00:33 +0300

    FIRST attempt

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,19 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe, {
-  loop: true,
-  fullscreen: true,
-  quality: '1080p',
+import _ from 'lodash';
+
+const player = new Player('vimeo-player', {
+  id: 236203659,
+  width: 640,
 });
 
 const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-const getCurrentTime = function (currentTime) {
-  const seconds = currentTime.seconds;
+player.on('timeupdate', _.throttle(getCurrentTime, 1000));
 
-  console.log(localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds)));
-};
-
-// player.on('timeupdate', throttle(getCurrentTime, 1000));
+function getCurrentTime(currentTime) {
+  const time = JSON.stringify(currentTime.seconds);
+  localStorage.setItem(CURRENT_TIME_KEY, time);
+}
 
 player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
-
-player
-  .setColor('#d8e0ff')
-  .then(function (color) {
-    console.log('The new color value: #D8E0FF');
-  })
-  .catch(function (error) {
-    console.log('An error occurred while setting the color');
-  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import _ from 'lodash';
 
+form = document.querySelector('.feedback-form');
+
+const LOCAL_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+console.log(formData);
+
+form.addEventListener('input', _.throttle(saveFormData, 500));
+
+function saveFormData(evt) {
+  formData[evt.target.name] = evt.target.value.trim();
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+reloadPage();
+
+function reloadPage() {
+  if (formData) {
+    const { email, message } = form.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
+
+form.addEventListener('submit', saveFormSubmit);
+
+function saveFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+  formData = {};
+}
